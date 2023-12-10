06c5ee40abf73cb28b254b4d981e0746fd47876b
André Moraes <andrelom@gmail.com>
2023-12-10 00:43:39 -0300

    Update rsa.sh

--- scripts/rsa.sh
+++ scripts/rsa.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
+name="pandora"
 path="./tmp"
-file="$path/pandora.pem"
 
 mkdir -p $path
 
 # Generate a private RSA key.
-openssl genpkey -algorithm RSA -out $file
+openssl genpkey -algorithm RSA -out "$path/pandora.pem"
 
 # Read the private key, remove newlines, and format as a single line.
-value=$(awk 'NF {printf "%s\\n", $0}' "$file")
+value=$(awk 'NF {printf "%s\\n", $0}' "$path/pandora.pem")
 
 # Save the formatted private key to a file.
-echo "$value" > "$file.txt"
+echo "$value" > "$path/pandora.inline.pem"
