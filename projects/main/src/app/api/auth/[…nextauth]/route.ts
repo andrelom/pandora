726c157b1c9f357270fe405f5dbca88c5b2ff83b
André Moraes <andrelom@gmail.com>
2023-12-10 20:31:52 -0300

    Update route.ts

--- projects/main/src/app/api/auth/[…nextauth]/route.ts
+++ projects/main/src/app/api/auth/[…nextauth]/route.ts
@@ -1,11 +1,9 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-
 import createNextAuth from 'next-auth'
 import createIdentityProvider from '@pandora/shared/identity/createIdentityProvider'
 import settings from '@pandora/shared/settings'
 
-function handler(req: NextApiRequest, res: NextApiResponse) {
-  return createNextAuth(req, res, {
+function handler(req: Request, res: Response) {
+  return createNextAuth({
     providers: [createIdentityProvider(req, res)],
     session: {
       maxAge: settings.identity.sessionExpirySeconds,
