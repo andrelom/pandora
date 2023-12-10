66887b523f1737a0666a5e302843e9268455653d
André Moraes <andrelom@gmail.com>
2023-12-10 20:10:22 -0300

    Create createIdentityProvider.ts

---
+++ packages/shared/src/identity/createIdentityProvider.ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import createCredentialsProvider from 'next-auth/providers/credentials'
+
+export default function createIdentityProvider(_req: NextApiRequest, res: NextApiResponse) {
+  return createCredentialsProvider({
+    id: 'identity',
+    name: 'Identity',
+    credentials: {
+      username: { label: 'Username', type: 'text' },
+      password: { label: 'Password', type: 'password' },
+    },
+    async authorize(credentials) {
+      // Any object returned will be saved in `user`
+      // property of the JWT.
+      return {
+        id: '',
+        name: '',
+        email: '',
+      }
+    },
+  })
+}
