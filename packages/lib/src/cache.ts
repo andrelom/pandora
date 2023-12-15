5d1f2ed7edd647d6867af7ff0adcd3338ce9ef81
André Moraes <andrelom@gmail.com>
2023-12-15 13:28:18 -0300

    Update cache.ts

--- packages/lib/src/cache.ts
+++ packages/lib/src/cache.ts
@@ -1,3 +1,6 @@
+import type { RedisClientType, RedisClientOptions } from 'redis'
+
+import { createClient } from 'redis'
 import { isNil } from '@pandora/lib/objects'
 
 export interface Cache {
@@ -58,12 +61,91 @@ export class MemoryCache implements Cache {
   }
 }
 
+export class RedisCache implements Cache {
+  private name: string
+  private memory: Map<string, any>
+  private redis: RedisClientType
+
+  constructor(name: string, options?: RedisClientOptions) {
+    this.name = name
+    this.memory = new Map()
+    this.redis = createClient(options) as RedisClientType
+  }
+
+  async get<T = any>(key: string): Promise<T | null> {
+    const id = this.getId(key)
+    const memory = this.memory.get(id)
+
+    if (!this.isNil(memory)) return memory as T
+
+    const value = await this.redis.get(id)
+
+    if (isNil(value)) return null
+
+    this.memorize(id, value)
+
+    return JSON.parse(value as string) as T
+  }
+
+  async set<T = any>(key: string, value: T, expiration: number = 1800): Promise<void> {
+    if (this.isNil(value)) return
+
+    const id = this.getId(key)
+
+    await this.redis.set(id, JSON.stringify(value))
+    await this.redis.expire(id, expiration)
+
+    this.memorize(id, value)
+  }
+
+  async delete(key: string): Promise<void> {
+    const id = this.getId(key)
+
+    await this.redis.del(id)
+
+    this.memory.delete(id)
+  }
+
+  async clear(prefix: string = ''): Promise<void> {
+    const keys = await this.redis.keys('*')
+
+    for (const key of keys) {
+      if (key.startsWith(this.getId(prefix))) {
+        await this.redis.del(key)
+
+        this.memory.delete(key)
+      }
+    }
+  }
+
+  private isNil<T = any>(value: T) {
+    return value === null || typeof value === 'undefined'
+  }
+
+  private getId(key: string) {
+    return `${this.name}:${key}`.toLowerCase()
+  }
+
+  // Debounce the cache reading routine.
+  // If requests are made within five seconds, the cache will be fresh in memory, if
+  // this time exceeds, we delete it, and we fetch the value from Redis.
+  private memorize<T = any>(id: string, value: T) {
+    if (this.isNil(value)) return
+
+    this.memory.set(id, value)
+
+    setTimeout(() => this?.memory?.delete(id), 5 * 1000)
+  }
+}
+
 export function createCache(name: string): Cache {
   const type = process.env.CACHE_TYPE
 
   switch (type) {
     case 'memory':
       return new MemoryCache(name)
+    case 'redis':
+      return new RedisCache(name)
     default:
       throw new Error(`invalid cache type (${type})`)
   }
