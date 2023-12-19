5905a7eb6e5b885f96f4c64c90a9abe8ceb87368
André Moraes <andrelom@gmail.com>
2023-12-19 17:17:57 -0300

    Update middleware.ts

--- projects/main/src/app/middleware.ts
+++ projects/main/src/app/middleware.ts
@@ -1,34 +1,20 @@
 import type { NextRequest } from 'next/server'
 
 import { NextResponse } from 'next/server'
-import logger from '@pandora/lib/logger/server'
-import jwt from '@pandora/lib/jwt'
-import api from '@pandora/lib/api'
-
-async function authorize(request: NextRequest) {
-  const pathname = request.nextUrl.pathname
-
-  if (!pathname.startsWith('/api')) return
-
-  const authorization = await jwt.authorize<{ pathname: string }>(request)
-
-  if (authorization.data?.pathname === pathname) return
-
-  logger.error('Middleware Authorize', authorization)
-
-  return api.getNotAuthorized({
-    trace: authorization.trace,
-  })
-}
+import pipeline from '@/pipeline'
 
 export const config = {
   matcher: '/:path*',
 }
 
 export async function middleware(request: NextRequest) {
-  const authorization = await authorize(request)
+  for (const stage of pipeline) {
+    const response = await stage(request)
 
-  if (authorization) return authorization
+    if (!response) continue
+
+    return response
+  }
 
   return NextResponse.next()
 }
