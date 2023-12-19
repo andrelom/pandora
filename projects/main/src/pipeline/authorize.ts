6661f7664184e3cfb9fc9e421c01eda971eb4432
André Moraes <andrelom@gmail.com>
2023-12-19 17:17:52 -0300

    Create authorize.ts

---
+++ projects/main/src/pipeline/authorize.ts
@@ -0,0 +1,21 @@
+import type { NextRequest, NextResponse } from 'next/server'
+
+import logger from '@pandora/lib/logger/server'
+import jwt from '@pandora/lib/jwt'
+import api from '@pandora/lib/api'
+
+export default async function authorize(request: NextRequest): Promise<NextResponse | null> {
+  const pathname = request.nextUrl.pathname
+
+  if (!pathname.startsWith('/api')) return null
+
+  const authorization = await jwt.authorize<{ pathname: string }>(request)
+
+  if (authorization.data?.pathname === pathname) return null
+
+  logger.error('Middleware Authorize', authorization)
+
+  return api.getNotAuthorized({
+    trace: authorization.trace,
+  })
+}
