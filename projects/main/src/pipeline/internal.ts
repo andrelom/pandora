13d362f2fa61c9df68a205565c1a24b8a95581ae
André Moraes <andrelom@gmail.com>
2023-12-20 16:30:21 -0300

    Update internal.ts

--- projects/main/src/pipeline/internal.ts
+++ projects/main/src/pipeline/internal.ts
@@ -1,5 +1,6 @@
 import type { Stage } from '.'
 
+import logger from '@pandora/lib/logger/server'
 import api from '@pandora/lib/api'
 
 const routes = new Set(process.env.INTERNAL_ROUTES)
@@ -12,6 +13,8 @@ const authorize: Stage = async (request) => {
   const secret = request.headers.get('x-internal-secret')
 
   if (secret === process.env.INTERNAL_SECRET) return null
+
+  logger.error('Middleware Internal: Invalid secret')
 
   return api.getNotAuthorized()
 }
