c20ed19ebc02aea1c185ea8ea909c2ad83abe7d7
André Moraes <andrelom@gmail.com>
2023-12-20 15:27:12 -0300

    Create route.ts

---
+++ projects/main/src/app/api/proxy/example/[...path]/route.ts
@@ -0,0 +1,24 @@
+import { createRouteProxy } from '@pandora/lib/proxy'
+
+const proxy = createRouteProxy({
+  target: 'https://example',
+  rewrite: {
+    '/api/proxy/example': '/v1',
+  },
+})
+
+export async function GET(request: Request) {
+  return await proxy(request)
+}
+
+export async function POST(request: Request) {
+  return await proxy(request)
+}
+
+export async function PUT(request: Request) {
+  return await proxy(request)
+}
+
+export async function DELETE(request: Request) {
+  return await proxy(request)
+}
