e1a74e3138bd59fa290692ad786f56eae5e94858
André Moraes <andrelom@gmail.com>
2023-12-20 16:15:47 -0300

    Update authorize.ts

--- projects/main/src/pipeline/authorize.ts
+++ projects/main/src/pipeline/authorize.ts
@@ -4,21 +4,22 @@ import { createURL } from '@pandora/lib/url'
 import http from '@pandora/lib/http'
 import api from '@pandora/lib/api'
 
-const routes = new Set(process.env.AUTHORIZATION_ROUTES)
+const routes = new Set(process.env.AUTHORIZE_ROUTES)
 
 const authorize: Stage = async (request) => {
   const source = request.nextUrl.pathname.toLowerCase()
 
-  if (!routes.has(source)) return api.getOk()
+  if (!routes.has(source)) null
 
   const url = createURL('/api/middleware/authorize')
   const data = {}
 
   const result = await http.post(url, data, {
+    internal: true,
     headers: request.headers,
   })
 
-  if (result.ok) return api.getOk()
+  if (result.ok) null
 
   return api.getNotAuthorized()
 }
