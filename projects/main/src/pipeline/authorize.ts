7b69c7ad10a995f775d92a2710add7021abdeaf7
André Moraes <andrelom@gmail.com>
2023-12-20 15:49:55 -0300

    Update authorize.ts

--- projects/main/src/pipeline/authorize.ts
+++ projects/main/src/pipeline/authorize.ts
@@ -1,7 +1,7 @@
 import type { Stage } from '.'
 
-import logger from '@pandora/lib/logger/server'
-import jwt from '@pandora/lib/jwt'
+import { createURL } from '@pandora/lib/url'
+import http from '@pandora/lib/http'
 import api from '@pandora/lib/api'
 
 const routes = new Set(process.env.AUTHORIZATION_ROUTES)
@@ -9,18 +9,18 @@ const routes = new Set(process.env.AUTHORIZATION_ROUTES)
 const authorize: Stage = async (request) => {
   const source = request.nextUrl.pathname.toLowerCase()
 
-  if (!routes.has(source)) return null
+  if (!routes.has(source)) return api.getOk()
 
-  const result = await jwt.authorize<{ pathname: string }>(request)
-  const target = result.data?.pathname.toLowerCase()
+  const url = createURL('/api/internal/authorize')
+  const data = {}
 
-  if (source === target) return null
-
-  logger.error('Middleware Authorize', result)
-
-  return api.getNotAuthorized({
-    trace: result.trace,
+  const result = await http.post(url, data, {
+    headers: request.headers,
   })
+
+  if (result.ok) return api.getOk()
+
+  return api.getNotAuthorized()
 }
 
 export default authorize
