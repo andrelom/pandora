69bb9cd6a0c0005097860ebba99347b4ace75bd7
André Moraes <andrelom@gmail.com>
2023-12-20 16:18:08 -0300

    Update index.ts

--- projects/main/src/pipeline/index.ts
+++ projects/main/src/pipeline/index.ts
@@ -1,9 +1,10 @@
 import type { NextRequest, NextResponse } from 'next/server'
 
+import internal from './internal'
 import authorize from './authorize'
 
 export type Stage = (request: NextRequest) => Promise<NextResponse | null>
 
-const pipeline: Array<Stage> = [authorize]
+const pipeline: Array<Stage> = [internal, authorize]
 
 export default pipeline
