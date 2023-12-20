daded68bdc57d918c8645a2ebfa46959b19fce75
André Moraes <andrelom@gmail.com>
2023-12-20 15:30:48 -0300

    Update route.ts

--- projects/main/src/app/api/proxy/example/[...path]/route.ts
+++ projects/main/src/app/api/proxy/example/[...path]/route.ts
@@ -1,9 +1,13 @@
+import { webcrypto } from 'crypto'
 import { createRouteProxy } from '@pandora/lib/proxy'
 
 const proxy = createRouteProxy({
   target: 'https://example',
   rewrite: {
     '/api/proxy/example': '/v1',
+  },
+  async middleware(request) {
+    request.headers.set('Authorization', `Bearer ${webcrypto.randomUUID()}`)
   },
 })
 
