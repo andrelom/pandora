71dd81da00e8adc24ba1fd79db0ee29a615c0909
André Moraes <andrelom@gmail.com>
2023-12-20 13:34:23 -0300

    Update route.ts

--- projects/main/src/app/api/hooks/revalidate/route.ts
+++ projects/main/src/app/api/hooks/revalidate/route.ts
@@ -1,6 +1,7 @@
 import { revalidatePath } from 'next/cache'
 import api, { parse } from '@pandora/lib/api'
 
+// @protected
 export async function POST(request: Request) {
   const data = await parse(request)
   const routes: Array<string> = data?.routes ?? []
@@ -9,5 +10,7 @@ export async function POST(request: Request) {
     revalidatePath(route, 'page')
   }
 
-  return api.getOk()
+  return api.getOk({
+    value: process.env.FILES_WITH_COMMENTS ?? '?',
+  })
 }
