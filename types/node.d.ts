6e60af828d0a246ce5ba2cdcba8ac774198d3e62
André Moraes <andrelom@gmail.com>
2023-12-20 16:15:30 -0300

    Update node.d.ts

--- types/node.d.ts
+++ types/node.d.ts
@@ -7,6 +7,8 @@ declare namespace NodeJS {
     readonly NODE_ENV: 'development' | 'production' | 'test'
     // Next.js
     readonly PUBLIC_URL: string
+    // Core
+    readonly INTERNAL_SECRET: string
     // Logger
     readonly LOGGER_LEVEL: string
     // Cache
